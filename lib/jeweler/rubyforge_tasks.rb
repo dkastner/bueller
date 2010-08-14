e0a9ee83c677baeffbfa5d3ceb017f77918084d1
Joshua Nichols <josh@technicalpickles.com>
2010-08-14 16:26:43 -0400

    Changed message to indicate rubyforge was deprecated, not that it wasn't supported at all.

--- lib/jeweler/rubyforge_tasks.rb
+++ lib/jeweler/rubyforge_tasks.rb
@@ -29,6 +29,8 @@ class Jeweler
     def initialize
       yield self if block_given?
 
+      $stderr.puts "Releasing gems to Rubyforge is deprecated. See details at http://wiki.github.com/technicalpickles/jeweler/migrating-from-releasing-gems-to-rubyforge"
+
       define
     end
 
