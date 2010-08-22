4b00d1a7ec54da9214d7cac127b1f71a204e61e9
Joshua Nichols <josh@technicalpickles.com>
2010-08-21 21:47:33 -0400

    Fixed 'jeweler --help' to not fail when .gitconfig is present.

--- lib/jeweler/generator/options.rb
+++ lib/jeweler/generator/options.rb
@@ -1,4 +1,3 @@
-
 class Jeweler
   class Generator
     class Options < Hash
@@ -11,7 +10,11 @@ class Jeweler
         self[:testing_framework]       = :rspec
         self[:documentation_framework] = :rdoc
 
-        git_config = Git.global_config
+        git_config =  if Pathname.new("~/.gitconfig").expand_path.exist?
+                        Git.global_config
+                      else
+                        {}
+                      end
         self[:user_name]       = ENV['GIT_AUTHOR_NAME']  || ENV['GIT_COMMITTER_NAME']  || git_config['user.name']
         self[:user_email]      = ENV['GIT_AUTHOR_EMAIL'] || ENV['GIT_COMMITTER_EMAIL'] || git_config['user.email']
         self[:github_username] = git_config['github.user']
