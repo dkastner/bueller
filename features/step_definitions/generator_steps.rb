1b9bc39f8bef0a2ffbad850143f345776a3d6b79
Ben Pickles <spideryoung@gmail.com>
2010-10-21 18:06:12 +0100

    Rcov: don't test coverage of gem dependencies by default.

--- features/step_definitions/generator_steps.rb
+++ features/step_definitions/generator_steps.rb
@@ -143,6 +143,13 @@ Then /^Rakefile has '(.*)' in the Rcov::RcovTask libs$/ do |libs|
   @rakefile_content.should =~ /#{block_variable}\.libs << '#{libs}'/
 end
 
+Then /^Rakefile has '(.*)' in the Rcov::RcovTask rcov_opts$/ do |rcov_opts|
+  @rakefile_content ||= File.read(File.join(@working_dir, @name, 'Rakefile'))
+  block_variable, task_block = yank_task_info(@rakefile_content, 'Rcov::RcovTask')
+
+  assert_match "#{block_variable}.rcov_opts << '#{rcov_opts}'", @rakefile_content
+end
+
 
 Then /^'(.*)' contains( regex)? '(.*)'$/ do |file, regex, expected_string|
   contents = File.read(File.join(@working_dir, @name, file))
